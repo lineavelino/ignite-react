3b41f7f6d40f6c6485c2dfb0d0799f4694a161e2
Aline Avelino <aline.savelino@gmail.com>
2021-07-22 17:33:13 -0300

    Ajuste símbolo comparação ===

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -62,7 +62,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     <RadioBox
                         type="button"
                         onClick={() => { setType('deposit'); }}
-                        isActive={type == 'deposit'}
+                        isActive={type === 'deposit'}
                         activeColor="green"
                     >
                         <img src={incomeImg} alt="Entrada" />
@@ -72,7 +72,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     <RadioBox
                         type="button"
                         onClick={() => { setType('withdraw'); }}
-                        isActive={type == 'withdraw'}
+                        isActive={type === 'withdraw'}
                         activeColor="red"
                     >
                         <img src={outcomeImg} alt="Saída" />
